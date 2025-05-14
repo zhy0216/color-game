9658263b16fd4b54ad0da5c7cb4766a1037d3fe4
Yang <zhy0216@gmail.com>
2025-05-15 00:41:18 +0800

    Add Prettier to project with configuration

--- src/game/main.ts
+++ src/game/main.ts
@@ -8,24 +8,17 @@ import { Preloader } from './scenes/Preloader';
 //  Find out more information about the Game Config at:
 //  https://docs.phaser.io/api-documentation/typedef/types-core#gameconfig
 const config: Phaser.Types.Core.GameConfig = {
-    type: AUTO,
-    width: 1024,
-    height: 768,
-    parent: 'game-container',
-    backgroundColor: '#028af8',
-    scene: [
-        Boot,
-        Preloader,
-        MainMenu,
-        MainGame,
-        GameOver
-    ]
+  type: AUTO,
+  width: 3200,
+  height: 1080,
+  zoom: 0.4,
+  parent: 'game-container',
+  backgroundColor: '#028af8',
+  scene: [Boot, Preloader, MainMenu, MainGame, GameOver],
 };
 
 const StartGame = (parent: string) => {
-
-    return new Game({ ...config, parent });
-
-}
+  return new Game({ ...config, parent });
+};
 
 export default StartGame;
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,5 @@
 import StartGame from './game/main';
 
 document.addEventListener('DOMContentLoaded', () => {
-
-    StartGame('game-container');
-
+  StartGame('game-container');
 });
